4db8a1b1e1c72570b79239710f4abc26898d6cdb
polancka <ana.polancec@gmail.com>
2023-12-30 11:59:20 +0100

    Added tunning logic

--- guitar_tuner/lib/tuning.dart
+++ guitar_tuner/lib/tuning.dart
@@ -1,6 +1,10 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_audio_capture/flutter_audio_capture.dart';
+import 'package:pitch_detector_dart/pitch_detector.dart';
+import 'package:pitchupdart/instrument_type.dart';
+import 'package:pitchupdart/pitch_handler.dart';
+import 'styles.dart';
 
 class TuningPage extends StatefulWidget {
   const TuningPage({super.key, required this.instrument, required this.tuning});
@@ -13,6 +17,55 @@ class TuningPage extends StatefulWidget {
 }
 
 class _TuningPageState extends State<TuningPage> {
+  final _audioRecorder = FlutterAudioCapture();
+  final pitchDetectorDart = PitchDetector(44100, 2000);
+  final pitchupDart = PitchHandler(InstrumentType.guitar);
+
+  var note = "";
+  var status = "Click on start";
+
+  void onError(Object e) {
+    print(e);
+  }
+
+  Future<void> startRecording() async {
+    await _audioRecorder.start(listener, onError,
+        sampleRate: 44100, bufferSize: 3000);
+    setState(() {
+      note = "";
+      status = "Play something";
+    });
+  }
+
+  Future<void> stopRecording() async {
+    await _audioRecorder.stop();
+    setState(() {
+      note = "";
+      status = "Click to start";
+    });
+  }
+
+  void listener(dynamic object) {
+    //Gets the audio sample
+    var buffer = Float64List.fromList(object.cast<double>());
+    final List<double> audioSample = buffer.toList();
+
+    //Uses pitch_detector_dart library to detect a pitch from the audio sample
+    final result = pitchDetectorDart.getPitch(audioSample);
+
+    //If there is a pitch - evaluate it
+    if (result.pitched) {
+      //Uses the pitchupDart library to check a given pitch for a Guitar
+      final handledPitchResult = pitchupDart.handlePitch(result.pitch);
+
+      //Updates the state with the result
+      setState(() {
+        note = handledPitchResult.note;
+        status = handledPitchResult.tuningStatus.toString();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +75,7 @@ class _TuningPageState extends State<TuningPage> {
             builder: (BuildContext context) {
               return IconButton(
                 icon: const Icon(Icons.arrow_back),
-                color: Colors.orange,
+                color: Styles.deepgreen,
                 onPressed: () {
                   Navigator.pop(context);
                 },
@@ -31,15 +84,14 @@ class _TuningPageState extends State<TuningPage> {
           ),
           backgroundColor: Colors.white.withOpacity(0.7),
         ),
-        backgroundColor: Colors.black,
         body: Container(
-          width: double.maxFinite,
-          height: double.maxFinite,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
           decoration: BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.bottomLeft,
               end: Alignment.topRight,
-              colors: [Colors.white, Colors.orange, Colors.white],
+              colors: [Styles.deepgreen, Styles.lightgreen, Styles.offwhite],
             ),
           ),
           child: Padding(
